7c11d876a2b89497b917a674d05bba08106011e5
Anoob Bava <anoob.bava@gmail.com>
2018-07-10 19:12:35 +0530

    implemented special attack

--- app.js
+++ app.js
@@ -11,11 +11,28 @@ new Vue({
             this.gameIsRunning = true;
             this.myHealth = 100;
             this.monsterHealth = 100;
+        },
+        attack: function(){
+            this.checkWin(5, 10);
+        },
+        specialAttack: function(){
+            this.checkWin(15, 15);
+        },
+        checkWin: function(mineScore, monsterScore){
+            var myValue = Math.floor(Math.random() * mineScore + 1);
+            var monsterValue = Math.floor(Math.random() * monsterScore + 1);
+            if ((this.myHealth - myValue) <= 0){
+                alert('monster Won');
+                this.startGame();
+            }
+            else 
+                this.myHealth -= myValue;
+            if ((this.monsterHealth - monsterValue) <= 0){
+                alert('You Won');
+                this.startGame();
+            }
+            else
+                this.monsterHealth -= monsterValue;
         }
-
-
-    },
-    computed: {
-
     }
-})
+});
