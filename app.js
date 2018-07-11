adb49a46d658656bde5729dd8b472ae70aec7a1c
Anoob Bava <anoob.bava@gmail.com>
2018-07-11 12:13:22 +0530

    completed the monster slayer

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ new Vue({
         myHealth: 100,
         monsterHealth: 100,
         gameIsRunning: false,
-        logDatas: []
+        logDatas: [],
+        // isPlayer: false
 
     },
     methods: {
@@ -12,6 +13,8 @@ new Vue({
             this.gameIsRunning = true;
             this.myHealth = 100;
             this.monsterHealth = 100;
+            this.logDatas = [];
+            // this.isPlayer = false;
         },
         attack: function(){
             this.checkWin(5, 10);
@@ -29,7 +32,8 @@ new Vue({
             else{
                 this.myHealth -= myValue;
                 this.logDatas.unshift({ 
-                    text: 'Player hits monster for' + myValue + 'points'
+                    isPlayer: false,
+                    text: 'Player hits monster for' + ' ' + myValue +' '+ 'points'
                 });
             }
             if ((this.monsterHealth - monsterValue) <= 0){
@@ -39,7 +43,8 @@ new Vue({
             else{
                 this.monsterHealth -= monsterValue;
                 this.logDatas.unshift({
-                    text: 'Monster hits Player for' + monsterValue + 'points'
+                    isPlayer: true,
+                    text: 'Monster hits Player for' +' '+ monsterValue +' '+ 'points'
                 });
             }
         },
@@ -48,9 +53,19 @@ new Vue({
             var monsterValue = Math.floor(Math.random() * 5 + 1)
             if ((myValue + this.myHealth) <= 100){
                 this.myHealth += myValue;
+                this.logDatas.unshift({
+                    isPlayer: false,
+                    text: 'heals Player for' +' '+ myValue +' '+ 'points'
+                });
+                // this.isPlayer = false;
             }
             if ((monsterValue + this.monsterHealth) <= 100){
                 this.monsterHealth += monsterValue;
+                this.logDatas.unshift({
+                    isPlayer: true,
+                    text: 'heals Monster for' +' '+ monsterValue +' '+ 'points'
+                });
+                // this.isPlayer = true;
             }
         },
         giveUp: function(){
